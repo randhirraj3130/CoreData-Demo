3f456fec633b4e69689aaf580d04ec89f29e0da2
randhirkushwaha93 <randhir.3023@gmail.com>
2019-08-17 21:34:36 +0530

    my first commit

--- BasicApp/AppDelegate.swift
+++ BasicApp/AppDelegate.swift
@@ -122,34 +122,17 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     func saveContext () {
         
-        if #available(iOS 10.0, *) {
-            
-            let context = persistentContainer.viewContext
-            if context.hasChanges {
-                do {
-                    try context.save()
-                } catch {
-                    // Replace this implementation with code to handle the error appropriately.
-                    // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                    let nserror = error as NSError
-                    fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
-                }
+        let context = persistentContainer.viewContext
+        if context.hasChanges {
+            do {
+                try context.save()
+                debugPrint("saved success!!!")
+            } catch {
+                // Replace this implementation with code to handle the error appropriately.
+                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+                let nserror = error as NSError
+                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
             }
-        } else {
-            // iOS 9.0 and below - however you were previously handling it
-            if managedObjectContext.hasChanges {
-                do {
-                    try managedObjectContext.save()
-                    print("saved success")
-                } catch {
-                    // Replace this implementation with code to handle the error appropriately.
-                    // abort() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                    let nserror = error as NSError
-                    NSLog("Unresolved error \(nserror), \(nserror.userInfo)")
-                    abort()
-                }
-            }
-            
         }
     }
     
--- BasicApp/DataManager.swift
+++ BasicApp/DataManager.swift
@@ -14,94 +14,61 @@ class DataManager: NSObject {
     static let shared = DataManager()
     
     func retunContext()->NSManagedObjectContext{
-        if #available(iOS 10.0, *) {
-            return (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
-        } else {
-            // Fallback on earlier versions
-           return (UIApplication.shared.delegate as! AppDelegate).managedObjectContext
-        }
+        return (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
     }
+    
     // MARK: - Core Data Saving support
     func saveStudentDetails(){
-
         
-       // var studentData:Student = Student()
-        if #available(iOS 10.0, *) {
-            let  studentData = Student(context: self.retunContext())
-        } else {
-            // Fallback on earlier versions
-            let entity = NSEntityDescription.entity(forEntityName: "Student", in: self.retunContext())!
-            
-           let studentData12 = Student(entity: entity, insertInto: self.retunContext())
-            studentData12.name = "randhir"
-            studentData12.roll_number = 0075
-            studentData12.school_name = "GNIOT"
-            studentData12.student_id = 1
-            studentData12.subject = 60.0
-            studentData12.student_class = "9"
-            
-            (UIApplication.shared.delegate as! AppDelegate).saveContext()
-            
-        }
-        
-
-//        // save data in local Db
-//        do {
-//            print("saveStudentDetails save success")
-//            try self.retunContext().save()
-//        } catch {
-//            print("Failed saving")
-//        }
+        //St
+//        let  studentData12 = Student(context: self.retunContext())
+//        studentData12.name = "raju"
+//        studentData12.roll_number = 0070
+//        studentData12.school_name = "gnit"
+//        studentData12.student_id = 1
+//        studentData12.student_class = "9"
+        (UIApplication.shared.delegate as! AppDelegate).saveContext()
     }
     // MARK: - Core Data Saving support
     func saveSubjeectDetails(){
 
-//        let context = self.persistentContainer.viewContext
+        let context = self.retunContext()
 //        let subjectContaxt = Subject(context: context)
-//
-//        subjectContaxt.hindi = "70"
-//        subjectContaxt.english = "70"
-//        subjectContaxt.maths = "70"
-//        subjectContaxt.science = "70"
-//        subjectContaxt.social_science = "70"
-
-       // let studentData = Student(context: context)
-
-
-//        studentData.name = "randhir"
-//        studentData.roll_number = 0075
-//        studentData.school_name = "GNIOT"
-//        studentData.student_id = 1
-//        studentData.subject = 60.0
-
-        // save data in local Db
-//        do {
-//            print("saveStudentDetails save success")
-//            try context.save()
-//        } catch {
-//            print("Failed saving")
-//        }
+//        subjectContaxt.hindi = "10"
+//        subjectContaxt.english = "20"
+//        subjectContaxt.maths = "30"
+//        subjectContaxt.science = "40"
+//        subjectContaxt.social_science = "50"
+//        subjectContaxt.student_id = 3
+        (UIApplication.shared.delegate as! AppDelegate).saveContext()
     }
-     // get table data in dictionary form
+     /// get table data in dictionary form
     func getTableDetails(tableName:String){
-        
         
         var dataArrray = [Any]()
         //let context = self.persistentContainer.viewContext
         let request = NSFetchRequest<NSFetchRequestResult>(entityName: tableName)
         do {
             let result = try self.retunContext().fetch(request)
+            
             for data in result as! [NSManagedObject] {
                 let keys = Array(data.entity.attributesByName.keys)
                 let dict = data.dictionaryWithValues(forKeys: keys)
+                
+                let john: Student = data as! Student
+               // let johnSubject = john.studentSubjectRelation?.hindi
+                //print("johnSubject--->>",johnSubject)
+                
                 dataArrray.append(dict)
             }
-            print("dataArrray--->>>",dataArrray)
+            
+           /// result.last
+           //let john: Student = result.first as! Student
+           //let johnSubject = john.studentSubjectRelation?.hindi
+            //print("johnSubject--->>",johnSubject)
+            //print("dataArrray--->>>",dataArrray)
         } catch {
             print("Failed")
         }
     }
-
-
-
 }
